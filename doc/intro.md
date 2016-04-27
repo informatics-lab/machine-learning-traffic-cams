3b14ec142ff7287e7daaa8944483771fcee7cb12
Niall Robinson <niall.h.robinson@gmail.com>
2016-04-27 14:33:23 +0100

    initial import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to molab-mysky-image-scraper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/molab_mysky_image_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns molab-mysky-image-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [molab-mysky-image-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
