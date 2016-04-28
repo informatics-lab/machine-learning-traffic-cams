11b19183e03e963f0ee2583313b5f1ae28234463
Niall Robinson <niall.h.robinson@gmail.com>
2016-04-28 17:17:27 +0100

    now passes file get test

--- src/molab_mysky_image_scraper/core.clj
+++ src/molab_mysky_image_scraper/core.clj
@@ -1,23 +1,26 @@
 (ns molab-mysky-image-scraper.core
   (:require [clojure.java.io :as io]
-            [clj-http.client :as client]
-            [clj-time.core :as time]
-            [clj-time.format :as time-format]
-            [semantic-csv.core :as sc :refer :all])
+    [clj-http.client :as client]
+    [clj-time.core :as time]
+    [clj-time.format :as time-format]
+    [semantic-csv.core :as sc :refer :all])
   (:gen-class))
 
 
 (defn blurp [url]
+    "Gets a bytestream from a URL object (such as an image)"
   (let [ba (java.io.ByteArrayOutputStream.)]
     (with-open [src (io/input-stream url)]
       (io/copy src ba))
     (.toByteArray ba)))
 
 (defn blit [bytestream file]
+    "Dumps a bystestream to a file"
    (with-open [w (java.io.BufferedOutputStream. (java.io.FileOutputStream. file))]
      (.write w bytestream)))
 
 (defn post-image
+    "Posts image and meta-data to the RESTful server"
     [file latitude longitude url]
     (client/post
         "https://iptd1v6we4.execute-api.eu-west-1.amazonaws.com/dev/image"
@@ -28,21 +31,31 @@
             {:name "longitude" :content longitude}
             {:name "image" :content (clojure.java.io/file file)}
             ]
-         :headers {"x-api-key" (System/getenv "API_KEY")}
+            :headers {"x-api-key" (System/getenv "API_KEY")}
         }
+        )
     )
-)
 
 (defn scrape-webcam
+    "Scrapes a webcam image and posts to the DB"
     [webcam]
     (let [image (blurp (:url webcam))]
-          (post-image image (:latitude webcam :longitude webcam))
+      (post-image image (:latitude webcam :longitude webcam))
+      )
     )
-)
 
-(defn webcams []
+(defn get-webcams []
+    "Loads list of webcams from our config file"
     (sc/slurp-csv "webcams.csv"))
 
+(defn scrape-webcams
+    "Loops over the webcams scraping them to the database"
+    ([webcams]
+        (recur (first webcams) (rest webcams)))
+    ([webcam webcams]
+        (scrape-webcam webcam)
+    )
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
