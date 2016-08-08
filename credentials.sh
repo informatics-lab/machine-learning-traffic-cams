c61e9a78556f58bff1a2b9df3efbd677f68459dd
ManonYG <manon.giraud@gmail.com>
2016-08-08 14:38:40 +0100

    Added example of credentials.sh

---
+++ credentials.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export AWS_ACCESS_KEY=
+export AWS_SECRET_ACCESS_KEY=
+export HELIOS_API_PATTERN=
+export HELIOS_API_ID=
+export HELIOS_API_SECRET=
+export DATAPOINT_KEY=
+export REGION=
+export BUCKET=
+export CSV_FILE=
+export TABLE=
