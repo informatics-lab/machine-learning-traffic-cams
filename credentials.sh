2d743c5229ad9ecb2b51c533c60763f2fb1a0c76
Antoine de Daran <antoine.de.daran@gmail.com>
2016-08-15 16:29:43 +0100

    Update credentials.sh

--- credentials.sh
+++ credentials.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-export HELIOS_API_PATTERN =
 export HELIOS_API_ID =
 export HELIOS_API_SECRET =
 
 export DATAPOINT_KEY =
 
+export REGION =
 export AWS_ACCESS_KEY =
 export AWS_SECRET_ACCESS_KEY =
-export REGION =
 export BUCKET =
 export TABLE =
 
